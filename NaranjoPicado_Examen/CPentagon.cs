6cd72228a7a96bf4c745a1f1bf7ef4828674cdef
jhonyagus <jhonyagus@hotmail.com>
2021-01-21 17:58:46 -0500

    Examen

---
+++ NaranjoPicado_Examen/CPentagon.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace NaranjoPicado_Examen
+{
+    class CPentagon
+    {
+        private Graphics mGraph;
+        private const float SF = 20;
+        private Pen mPen, mPen1;
+        private PointF A, B, C, D, E, F, G, H, I, J, K, L, M, N, O;
+        private float mSide, mAngle, mAngleA, mA, mB, mC, mD;
+
+        public CPentagon()
+        {
+            mSide = 12.0f;
+        }
+
+        private PointF HalfPoint(PointF A, PointF B)
+        {
+            return new PointF((A.X + B.X)/2,(A.Y+B.Y)/2); 
+        }
+
+        private void CalculateVertex()
+        {
+            mAngle = 36.0f * (float)Math.PI / 180.0f;
+            mAngleA = 72.0f * (float)Math.PI / 180.0f;
+            mB = mSide * (float)Math.Cos(mAngle);
+            mA = mSide * (float)Math.Sin(mAngle);
+            mC = mSide * (float)Math.Sin(mAngleA);
+            mD = mSide * (float)Math.Cos(mAngleA);
+
+            A.X = mB; A.Y = 0;
+            B.X = 0; B.Y = mA;
+            C.X = 2.0f * mB; C.Y = mA;
+            D.X = mD; D.Y = mA + mC;
+            E.X = mD + mSide; E.Y = mA + mC;
+            F.X = A.X / 2; F.Y = B.Y / 2;
+            G.X = 1.5f * mB; G.Y = 0.5f * mA;
+            H.X = D.X / 2; H.Y = mA + 0.5f * mC;
+            I.X = C.X - 0.5f * mD; I.Y = H.Y;
+            J.X = mD + 0.5f * mSide; J.Y = mA + mC;
+            K = HalfPoint(B, C);
+            L = HalfPoint(A, D);
+            M = HalfPoint(A, E);
+            N = HalfPoint(B, E);
+            O = HalfPoint(D, C);
+        }
+
+        public void GraphShape(PictureBox picCanvas)
+        {
+            mGraph = picCanvas.CreateGraphics();
+
+            CalculateVertex();
+
+            mPen = new Pen(Color.Blue, 2.0f);
+            mGraph.DrawLine(mPen, A.X * SF, A.Y * SF, B.X * SF, B.Y * SF);
+            mGraph.DrawLine(mPen, B.X * SF, B.Y * SF, D.X * SF, D.Y * SF);
+            mGraph.DrawLine(mPen, C.X * SF, C.Y * SF, A.X * SF, A.Y * SF);
+            mGraph.DrawLine(mPen, D.X * SF, D.Y * SF, E.X * SF, E.Y * SF);
+            mGraph.DrawLine(mPen, E.X * SF, E.Y * SF, C.X * SF, C.Y * SF);
+
+            mGraph.DrawLine(mPen, A.X * SF, A.Y * SF, D.X * SF, D.Y * SF);
+            mGraph.DrawLine(mPen, A.X * SF, A.Y * SF, H.X * SF, H.Y * SF);
+            mGraph.DrawLine(mPen, A.X * SF, A.Y * SF, E.X * SF, E.Y * SF);
+            mGraph.DrawLine(mPen, A.X * SF, A.Y * SF, I.X * SF, I.Y * SF);
+
+            mGraph.DrawLine(mPen, B.X * SF, B.Y * SF, G.X * SF, G.Y * SF);
+            mGraph.DrawLine(mPen, B.X * SF, B.Y * SF, C.X * SF, C.Y * SF);
+            mGraph.DrawLine(mPen, B.X * SF, B.Y * SF, J.X * SF, J.Y * SF);
+            mGraph.DrawLine(mPen, B.X * SF, B.Y * SF, E.X * SF, E.Y * SF);
+            
+            mGraph.DrawLine(mPen, C.X * SF, C.Y * SF, F.X * SF, F.Y * SF);
+            mGraph.DrawLine(mPen, C.X * SF, C.Y * SF, B.X * SF, B.Y * SF);
+            mGraph.DrawLine(mPen, C.X * SF, C.Y * SF, D.X * SF, D.Y * SF);
+            mGraph.DrawLine(mPen, C.X * SF, C.Y * SF, J.X * SF, J.Y * SF);
+
+            mGraph.DrawLine(mPen, D.X * SF, D.Y * SF, F.X * SF, F.Y * SF);
+            mGraph.DrawLine(mPen, D.X * SF, D.Y * SF, I.X * SF, I.Y * SF);
+
+            mGraph.DrawLine(mPen, E.X * SF, E.Y * SF, G.X * SF, G.Y * SF);
+            mGraph.DrawLine(mPen, E.X * SF, E.Y * SF, H.X * SF, H.Y * SF);
+
+            mGraph.DrawLine(mPen, F.X * SF, F.Y * SF, J.X * SF, J.Y * SF);
+            mGraph.DrawLine(mPen, F.X * SF, F.Y * SF, I.X * SF, I.Y * SF);
+
+            mGraph.DrawLine(mPen, G.X * SF, G.Y * SF, H.X * SF, H.Y * SF);
+            mGraph.DrawLine(mPen, G.X * SF, G.Y * SF, J.X * SF, J.Y * SF);
+
+            mGraph.DrawLine(mPen, H.X * SF, H.Y * SF, I.X * SF, I.Y * SF);
+
+            mGraph.DrawLine(mPen, A.X * SF, A.Y * SF, K.X * SF, K.Y * SF);
+            mGraph.DrawLine(mPen, B.X * SF, B.Y * SF, L.X * SF, L.Y * SF);
+            mGraph.DrawLine(mPen, C.X * SF, C.Y * SF, M.X * SF, M.Y * SF);
+            mGraph.DrawLine(mPen, D.X * SF, D.Y * SF, N.X * SF, N.Y * SF);
+            mGraph.DrawLine(mPen, E.X * SF, E.Y * SF, O.X * SF, O.Y * SF);
+        }
+    }
+}
---
+++ NaranjoPicado_Examen/frmPentagon.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace NaranjoPicado_Examen
+{
+    partial class frmPentagon
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Gráfico = new System.Windows.Forms.GroupBox();
+            this.grbButtons = new System.Windows.Forms.GroupBox();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.Gráfico.SuspendLayout();
+            this.grbButtons.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Gráfico
+            // 
+            this.Gráfico.Controls.Add(this.picCanvas);
+            this.Gráfico.Location = new System.Drawing.Point(13, 13);
+            this.Gráfico.Name = "Gráfico";
+            this.Gráfico.Size = new System.Drawing.Size(629, 425);
+            this.Gráfico.TabIndex = 0;
+            this.Gráfico.TabStop = false;
+            this.Gráfico.Text = "grbCanvas";
+            // 
+            // grbButtons
+            // 
+            this.grbButtons.Controls.Add(this.btnExit);
+            this.grbButtons.Controls.Add(this.btnReset);
+            this.grbButtons.Controls.Add(this.btnDraw);
+            this.grbButtons.Location = new System.Drawing.Point(662, 23);
+            this.grbButtons.Name = "grbButtons";
+            this.grbButtons.Size = new System.Drawing.Size(129, 414);
+            this.grbButtons.TabIndex = 1;
+            this.grbButtons.TabStop = false;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(20, 19);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(97, 23);
+            this.btnDraw.TabIndex = 0;
+            this.btnDraw.Text = "Graficar";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(20, 48);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(97, 23);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Resetear";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(20, 77);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(97, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Salir";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.Location = new System.Drawing.Point(125, 19);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(400, 400);
+            this.picCanvas.TabIndex = 0;
+            this.picCanvas.TabStop = false;
+            // 
+            // frmPentagon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grbButtons);
+            this.Controls.Add(this.Gráfico);
+            this.Name = "frmPentagon";
+            this.Text = "Pentagono";
+            this.Gráfico.ResumeLayout(false);
+            this.grbButtons.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Gráfico;
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.GroupBox grbButtons;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnDraw;
+    }
+}
+
